25b07031c01049d1ba7207ce5696eb3132de55b1
Luis Rojas <rojas@icomp.ufam.edu.br>
2024-04-26 06:40:57 -0400

    Add vercel analytics
    Signed-off-by: Luis Rojas <rojas@icomp.ufam.edu.br>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ExpensesContextProvider } from "./context/store";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         <ExpensesContextProvider>{children}</ExpensesContextProvider>
       </body>
     </html>
